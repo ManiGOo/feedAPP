487bf84470290005a88658fc69aeedff87ffad83
Manish <its.nathmanish@gmail.com>
2025-08-29 23:39:20 +0530

    Added icon

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API_URL,
 });
 
 // Attach accessToken automatically
@@ -24,7 +26,7 @@ api.interceptors.response.use(
 
       try {
         const refresh = localStorage.getItem("refreshToken");
-        const res = await axios.post("http://localhost:5000/api/auth/refresh", { refreshToken: refresh });
+        const res = await axios.post(`${API_URL}/auth/refresh`, { refreshToken: refresh });
 
         localStorage.setItem("accessToken", res.data.accessToken);
 
